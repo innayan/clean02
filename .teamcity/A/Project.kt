0b5c51a095acce0d2970acf87bbafaa42e51f580
admin <admin@TeamCity>
2019-12-09 16:15:02 +0300

    TeamCity change in 'A' project: Synchronization with own VCS root is enabled

---
+++ .teamcity/A/Project.kt
@@ -0,0 +1,61 @@
+package A
+
+import A.buildTypes.*
+import A.vcsRoots.*
+import A.vcsRoots.A_HttpsGithubComInnayanClean02
+import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.Project
+import jetbrains.buildServer.configs.kotlin.v2019_2.projectFeatures.VersionedSettings
+import jetbrains.buildServer.configs.kotlin.v2019_2.projectFeatures.versionedSettings
+
+object Project : Project({
+    uuid = "63192156-e7c6-427c-875a-e9954324aa54"
+    id("A")
+    parentId("_Root")
+    name = "A"
+
+    vcsRoot(A_HttpsGithubComInnayanClean02)
+
+    buildType(A_ConfigBCopy)
+    buildType(B_Bb)
+
+    features {
+        versionedSettings {
+            id = "PROJECT_EXT_2"
+            mode = VersionedSettings.Mode.ENABLED
+            buildSettingsMode = VersionedSettings.BuildSettingsMode.USE_CURRENT_SETTINGS
+            rootExtId = "${A_HttpsGithubComInnayanClean02.id}"
+            showChanges = false
+            settingsFormat = VersionedSettings.Format.KOTLIN
+            storeSecureParamsOutsideOfVcs = true
+        }
+    }
+
+    cleanup {
+        keepRule {
+            id = "KEEP_RULE_4"
+            keepAtLeast = allBuilds()
+            applyToBuilds {
+                withTags = anyOf("a1", "gg", "test")
+            }
+            dataToKeep = everything()
+            applyPerEachBranch = true
+            preserveArtifactsDependencies = true
+        }
+        keepRule {
+            id = "KEEP_RULE_5"
+            keepAtLeast = allBuilds()
+            applyToBuilds {
+                withTags = anyOf("a2")
+            }
+            dataToKeep = everything()
+            applyPerEachBranch = true
+            preserveArtifactsDependencies = true
+        }
+        baseRule {
+            all(builds = 123)
+            history(builds = 123)
+            artifacts(builds = 123)
+        }
+    }
+})
---
+++ .teamcity/A/buildTypes/B_Bb.kt
@@ -0,0 +1,21 @@
+package A.buildTypes
+
+import jetbrains.buildServer.configs.kotlin.v2019_2.*
+
+object B_Bb : BuildType({
+    uuid = "beb4521c-3bc7-47cb-9c07-d03f1409464a"
+    name = "BB"
+
+    cleanup {
+        keepRule {
+            id = "KEEP_RULE_2"
+            keepAtLeast = allBuilds()
+            applyToBuilds {
+                withTags = anyOf("b2", "over")
+            }
+            dataToKeep = everything()
+            applyPerEachBranch = true
+            preserveArtifactsDependencies = true
+        }
+    }
+})
